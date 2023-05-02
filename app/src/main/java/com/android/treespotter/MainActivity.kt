c478df08b474b2620d9e25e5d37e0c0065fdb827
matt <emal>
2023-05-02 09:48:34 -0500

    Video 4 finished. Created xml files and main layout.

--- app/src/main/java/com/android/treespotter/MainActivity.kt
+++ app/src/main/java/com/android/treespotter/MainActivity.kt
@@ -3,6 +3,7 @@ package com.android.treespotter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -14,49 +15,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Making database
-        val db = Firebase.firestore
+        showFragment("MAP")
 
-        // Example data
-//        val tree = mapOf("name" to "pine", "dateSpotted" to Date())
-//        db.collection("trees").add(tree)
-//
-//        val tree2 = mapOf("name" to "oak", "dateSpotted" to Date())
-//        db.collection("trees").add(tree2)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
-        // Making a Tree object
-            // fromTree.kt
-//        val tree = Tree("Pine", Date())
-//        db.collection("trees").add(tree)
-
-        // Getting all of the data on the DB
-//        db.collection("trees").get().addOnSuccessListener { treeDocuments ->
-        // Snapshot monitors all of the documents for changes and updates
-        db.collection("trees")
-            .whereEqualTo("name", "Pine")           // Filtering out values/documents.
-            .whereEqualTo("favorite", true)         // Add as many as wanted
-            .orderBy("dateSpotted", Query.Direction.DESCENDING)// Direction in descending order
-            .limit(3)
-            .addSnapshotListener { treeDocuments, error ->
-
-            // Check for errors, no data
-            if (error != null) {
-                Log.e(TAG, "Error getting all trees.", error)
-            }
-            if (treeDocuments != null) {
-                // Looping over each data set
-                for (treeDoc in treeDocuments) {
-                    val treeFromFirebase = treeDoc.toObject(Tree::class.java)
-//                    val name = treeDoc["name"]
-//                    val dateSpotted = treeDoc["dateSpotted"]
-//                    val favorite = treeDoc["favorite"]
-                    // Where is the path?
-                    // Shows in Logcat the document id from Firestore
-                    val path = treeDoc.reference.path
-
-                    Log.d(TAG, "$treeFromFirebase, $path")
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.show_map -> {
+                    showFragment("MAP")
+                    true
+                }
+                R.id.show_list-> {
+                    showFragment("LIST")
+                    true
+                } else -> {
+                    false
                 }
             }
+        }
+    }
+    // Function to show either the map or the list
+    private fun showFragment(tag: String) {
+        // If we are not seeing the fragment with the given tag, display it
+        if (supportFragmentManager.findFragmentByTag(tag) == null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            when (tag) {
+                "MAP" -> transaction.replace(R.id.fragmentContainerView, TreeMapFragment.newInstance(), "MAP")
+                "LIST" -> transaction.replace(R.id.fragmentContainerView, TreeListFragment.newInstance(), "LIST")
+            }
+            // Makes the changes
+            transaction.commit()
         }
     }
 }
